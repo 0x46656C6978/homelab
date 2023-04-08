5b744bc50d4ad96e7029979a0dcf6eb7f8b1e443
0x46656C6978 <admin@iamfelix.dev>
2023-04-08 23:26:45 +0700

    feat: update master-node.sh

--- setup/master-node.sh
+++ setup/master-node.sh
@@ -10,15 +10,19 @@ HOST_IP=""
 
 if [[ $UNAMECHK == "Darwin" ]]; then
   HOST_IP=$(ifconfig | grep "inet " | grep -v  "127.0.0.1" | awk -F " " '{print $2}'|head -n1)
-  # echo $EXTERNAL_IP
 else
   HOST_IP=$(ip a | grep "inet " | grep -v  "127.0.0.1" | awk -F " " '{print $2}'|awk -F "/" '{print $1}'|head -n1)
-  # echo $EXTERNAL_IP
 fi
 
 blue "Host IP: $HOST_IP"
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL="v1.25.7+k3s1" sh -
+if ! [ -x "$(command -v helm)"]; then
+  red "helm is not installed. Installing now..."
+  curl -sfL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | sh
+fi
+
+
+curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL="v1.25.7+k3s1" INSTALL_K3S_EXEC="server --disable=traefik" sh -
 
 blue "Generate kube_config file (root permission required)"
 sudo cp /etc/rancher/k3s/k3s.yaml ./kube_config
