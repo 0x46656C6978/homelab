ebf13ff6223bb6dd26f66e76e7074e54d7ff4ec5
0x46656C6978 <nguyenthanhdat.js@gmail.com>
2023-08-05 01:19:33 +0700

    feat: add jenkins

--- k8s/workloads/deploy.sh
+++ k8s/workloads/deploy.sh
@@ -2,4 +2,8 @@
 helmfile repos
 helmfile apply
 
+# Get grafana admin password
 # kubectl get secret --namespace system-monitor grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
+
+# Get jenkins admin password
+# kubectl exec --namespace jenkins -it svc/jenkins -c jenkins -- /bin/cat /run/secrets/additional/chart-admin-password && echo
